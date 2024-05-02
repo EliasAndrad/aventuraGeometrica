b47dbb3ca385173e7062a3dbe040df2f0cf8b528
Elias Andrade <84996872+EliasAndrad@users.noreply.github.com>
2024-05-02 16:14:49 -0300

    Corrigindo erros

--- scripts/index.js
+++ scripts/index.js
@@ -18,3 +18,56 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+function calcularAreaCirculo() {
+    console.log("Função calcularArea() chamada");
+    var raio = parseFloat(document.getElementById('raio').value);
+    console.log("Valor do raio:", raio);
+    var area = raio * raio * Math.PI;
+    console.log("Área calculada:", area);
+    document.getElementById('resultado').value = area.toFixed(2);
+}
+
+
+function calculoAreaCubo() {
+    var base = parseFloat(document.getElementById('base').value);
+    var area = 6 * base * base;
+    document.getElementById('resultado').value = area;
+}
+
+function calculoAreaCuboide(){
+    var comprimento = parseFloat(document.getElementById('comprimento').value);
+    var altura = parseFloat(document.getElementById('altura').value);
+    var largura = parseFloat(document.getElementById('largura').value);
+
+
+    var area =  2 * (comprimento * altura) + 2 * (altura * largura) + 2 * (largura * comprimento)
+
+    document.getElementById('resultado').value = area;
+    
+}
+
+
+
+function calculoAreaQuadrado(){
+    var base = parseFloat(document.getElementById('base').value);
+    var altura = parseFloat(document.getElementById('altura').value);
+
+    var area = base * altura;
+
+    document.getElementById('resultado').value = area;
+    
+}
+
+
+function calculoAreaTriangulo(){
+    var base = parseFloat(document.getElementById('base').value);
+    var altura = parseFloat(document.getElementById('altura').value);
+
+    var area = (base * altura)/2;
+
+    document.getElementById('resultado').value = area;
+    
+}
